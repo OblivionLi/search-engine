bbb1a90b5c4de89385bcd764df7bdbed7cb63ecb
Liviu Andrei <ic01oblivionp@gmail.com>
2020-01-16 18:34:45 +0200

    Change SQL code

--- crawl.php
+++ crawl.php
@@ -10,59 +10,49 @@ $alreadyFoundImages = array();
 function linkExists($url)
 {
     global $db;
+    $stmt = mysqli_prepare($db, "SELECT * FROM sites WHERE url = ?");
 
-    $sql = "SELECT * FROM sites WHERE url = '$url'";
+    $stmt->bind_param('s', $url);
+    $stmt->execute();
 
-    $result = mysqli_query($db, $sql);
-
-    if (!$result) {
-        confirm_query_result($result);
-    }
-
-    return $result->num_rows != 0;
+    return $stmt->num_rows() != 0;
 }
 
 function insertLink($url, $title, $description, $keywords)
 {
     global $db;
 
-    $sql = "INSERT INTO sites (url, title, description, keywords) VALUES ('$url', '$title', '$description', '$keywords')";
+    $stmt = mysqli_prepare($db, "INSERT INTO sites (url, title, description, keywords) VALUES (?, ?, ?, ?)");
 
-    $result = mysqli_query($db, $sql);
-
-    if (!$result) {
-        confirm_query_result($result);
-    }
+    $stmt->bind_param('ssss', $url, $title, $description, $keywords);
+    $stmt->execute();
 }
 
 function insertImage($url, $src, $alt, $title)
 {
     global $db;
 
-    $sql = "INSERT INTO images (siteUrl, imageUrl, alt, title) VALUES ('$url', '$src', '$alt', '$title')";
+    $stmt = mysqli_prepare($db, "INSERT INTO images (siteUrl, imageUrl, alt, title) VALUES (?, ?, ?, ?)");
 
-    $result = mysqli_query($db, $sql);
-
-    if (!$result) {
-        confirm_query_result($result);
-    }
+    $stmt->bind_param('ssss', $url, $src, $alt, $title);
+    $stmt->execute();
 }
 
 function createLink($src, $url)
 {
-    $scheme = parse_url($url)['scheme'];
-    $host = parse_url($url)['host'];
+    $scheme = parse_url($url)["scheme"];
+    $host = parse_url($url)["host"];
 
-    if (substr($src, 0, 2) == '//') {
-        $src = $scheme . ':' . $src;
-    } elseif (substr($src, 0, 1) == '/') {
-        $src = $scheme . '://' . $host . $src;
-    } elseif (substr($src, 0, 2) == './') {
-        $src = $scheme . '://' . $host . dirname(parse_url($url)['path']) . substr($src, 1);
-    } elseif (substr($src, 0, 3) == '../') {
-        $src = $scheme . '://' . $host . '/' . $src;
-    } elseif (substr($src, 0, 5) != 'https' && substr($src, 0, 4) != 'http') {
-        $src = $scheme . '://' . $host . '/' . $src;
+    if (substr($src, 0, 2) == "//") {
+        $src = $scheme . ":" . $src;
+    } else if (substr($src, 0, 1) == "/") {
+        $src = $scheme . "://" . $host . $src;
+    } else if (substr($src, 0, 2) == "./") {
+        $src = $scheme . "://" . $host . dirname(parse_url($url)["path"]) . substr($src, 1);
+    } else if (substr($src, 0, 3) == "../") {
+        $src = $scheme . "://" . $host . "/" . $src;
+    } else if (substr($src, 0, 5) != "https" && substr($src, 0, 4) != "http") {
+        $src = $scheme . "://" . $host . "/" . $src;
     }
 
     return $src;
@@ -74,50 +64,52 @@ function getDetails($url)
 
     $parser = new DomDocumentParser($url);
 
-    $titleArray = $parser->getTitle();
+    $titleArray = $parser->getTitleTags();
 
     if (sizeof($titleArray) == 0 || $titleArray->item(0) == NULL) {
         return;
     }
 
     $title = $titleArray->item(0)->nodeValue;
-    $title = str_replace('\n', '', $title);
+    $title = str_replace("\n", "", $title);
 
-    if ($title == '') {
+    if ($title == "") {
         return;
     }
 
-    $description = '';
-    $keywords = '';
+    $description = "";
+    $keywords = "";
 
-    $metaArray = $parser->getMetaTags();
+    $metasArray = $parser->getMetatags();
 
-    foreach ($metaArray as $meta) {
-        if ($meta->getAttribute('name') == 'description') {
-            $description = $meta->getAttribute('content');
+    foreach ($metasArray as $meta) {
+
+        if ($meta->getAttribute("name") == "description") {
+            $description = $meta->getAttribute("content");
         }
-        if ($meta->getAttribute('name') == 'keywords') {
-            $keywords = $meta->getAttribute('content');
+
+        if ($meta->getAttribute("name") == "keywords") {
+            $keywords = $meta->getAttribute("content");
         }
     }
 
-    $description = str_replace('\n', '', $description);
-    $keywords = str_replace('\n', '', $keywords);
+    $description = str_replace("\n", "", $description);
+    $keywords = str_replace("\n", "", $keywords);
+
 
     if (linkExists($url)) {
-        echo 'already exists';
-    } elseif (insertLink($url, $title, $description, $keywords)) {
-        echo 'success';
+        echo "$url already exists<br>";
+    } else if (insertLink($url, $title, $description, $keywords)) {
+        echo "SUCCESS: $url<br>";
     } else {
-        echo 'error: failed to insert';
+        echo "ERROR: Failed to insert $url<br>";
     }
 
     $imageArray = $parser->getImages();
-
     foreach ($imageArray as $image) {
-        $src = $image->getAttribute('src');
-        $alt = $image->getAttribute('alt');
-        $title = $image->getAttribute('title');
+        $src = $image->getAttribute("src");
+        $alt = $image->getAttribute("alt");
+        $title = $image->getAttribute("title");
 
         if (!$title && !$alt) {
             continue;
@@ -130,7 +122,10 @@ function getDetails($url)
 
             insertImage($url, $src, $alt, $title);
         }
+
     }
+
+
 }
 
 function followLinks($url)
@@ -143,15 +138,17 @@ function followLinks($url)
     $linkList = $parser->getLinks();
 
     foreach ($linkList as $link) {
-        $href = $link->getAttribute('href');
+        $href = $link->getAttribute("href");
 
-        if (strpos($href, '#') !== false) {
+        if (strpos($href, "#") !== false) {
             continue;
-        } elseif (substr($href, 0, 11) == 'javascript:') {
+        } else if (substr($href, 0, 11) == "javascript:") {
             continue;
         }
 
+
         $href = createLink($href, $url);
+
 
         if (!in_array($href, $alreadyCrawled)) {
             $alreadyCrawled[] = $href;
@@ -159,6 +156,7 @@ function followLinks($url)
 
             getDetails($href);
         }
+
     }
 
     array_shift($crawling);
@@ -166,7 +164,8 @@ function followLinks($url)
     foreach ($crawling as $site) {
         followLinks($site);
     }
+
 }
 
-$startUrl = "https://www.bbc.com/news";
+$startUrl = "https://en.wikipedia.org/wiki/Main_Page";
 followLinks($startUrl);
